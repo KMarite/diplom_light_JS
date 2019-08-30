d80eae5e2e833b783dcf7265073a2fc148640611
Marina <kravchenko.marite@gmail.com>
2019-08-31 01:19:03 +0300

    написан скрипт для акоордеона 2, но нужно доработать

--- script.js
+++ script.js
@@ -10,33 +10,73 @@ window.addEventListener('DOMContentLoaded', () => {
             popupClose = document.querySelector('.popup-close'),
             popupContent = popupCall.querySelector('.popup-content');
 
-            callBtn.forEach((elem) => {
-                elem.addEventListener('click', () => {
-                    popupCall.style.display = 'block';                 
-                });
+        callBtn.forEach((elem) => {
+            elem.addEventListener('click', () => {
+                popupCall.style.display = 'block';
             });
-            popupClose.addEventListener('click', () => {              
+        });
+        popupClose.addEventListener('click', () => {
+            popupCall.style.display = 'none';
+            event.preventDefault();
+
+        });
+        popupCall.addEventListener('click', (event) => {
+            let target = event.target;
+            if (target.classList.contains('popup-close')) {
                 popupCall.style.display = 'none';
-                event.preventDefault();
-            
-            });
-            popupCall.addEventListener('click', (event) => {
-                let target = event.target;
-                if (target.classList.contains('popup-close')) {
+                popupContent.removeAttribute('style');
+            } else {
+                target = target.closest('.popup-content');
+                if (!target) {
                     popupCall.style.display = 'none';
-                    popupContent.removeAttribute('style');
-                } else {
-                    target = target.closest('.popup-content');
-                    if (!target) {
-                        popupCall.style.display = 'none';
-                    }
                 }
-            });
-             
+            }
+        });
+
     };
     togglePopup();
 
+    // аккордеон 2
 
+    const accordion = () => {
+        const accordionTwo = document.getElementById('accordion-two'),
+            panel = accordionTwo.querySelectorAll('.panel-heading');
+
+        panel.forEach((item) => {
+            item.addEventListener('click', () => {
+                item.classList.contains('in');
+                let content = item.nextElementSibling;
+                if (content.classList.toggle('in')) {
+                    content.style.display = 'none';
+                } else {
+                    content.style.display = 'block';
+                }
+           
+                panel.forEach((elem) => {
+                    let contentOtherPanel = elem.nextElementSibling;
+                    if(!(contentOtherPanel.classList.toggle('in'))) {
+                        contentOtherPanel.style.display = 'none';
+                    }
+            
+                });           
+
+            });
+
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+    };
+    accordion();
 
 
 

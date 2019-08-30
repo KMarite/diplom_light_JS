eb83718813c11b90df077595245a329087550dfd
Marina <kravchenko.marite@gmail.com>
2019-08-30 19:51:46 +0300

    сделано модальное окно

---
+++ script.js
@@ -0,0 +1,71 @@
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    // модальное окно
+
+    const togglePopup = () => {
+
+        const callBtn = document.querySelectorAll('.call-btn'),
+            popupCall = document.querySelector('.popup-call'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = popupCall.querySelector('.popup-content');
+
+            callBtn.forEach((elem) => {
+                elem.addEventListener('click', () => {
+                    popupCall.style.display = 'block';                 
+                });
+            });
+            popupClose.addEventListener('click', () => {              
+                popupCall.style.display = 'none';
+            
+            });
+            popupCall.addEventListener('click', (event) => {
+                let target = event.target;
+                if (target.classList.contains('popup-close')) {
+                    popupCall.style.display = 'none';
+                    popupContent.removeAttribute('style');
+                } else {
+                    target = target.closest('.popup-content');
+                    if (!target) {
+                        popupCall.style.display = 'none';
+                    }
+                }
+            });
+             
+    };
+    togglePopup();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

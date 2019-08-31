dc075ae4d38f93c161310403bcb57222540922ed
Marina <kravchenko.marite@gmail.com>
2019-09-01 00:07:40 +0300

    модальнoe окнo popup-check

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    // модальное окно
+    // модальнoe окнo popup-call
 
     const togglePopup = () => {
 
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
             popupCall = document.querySelector('.popup-call'),
             popupClose = document.querySelector('.popup-close'),
             popupContent = popupCall.querySelector('.popup-content');
-
+       
         callBtn.forEach((elem) => {
             elem.addEventListener('click', () => {
                 popupCall.style.display = 'block';
@@ -36,10 +36,46 @@ window.addEventListener('DOMContentLoaded', () => {
     };
     togglePopup();
 
+     // модальнoe окнo popup-check
+
+     const check = () => {
+
+        const checkBtn = document.querySelectorAll('.check-btn'),
+            popupCheck = document.querySelector('.popup-check'),
+            popupClose = popupCheck.querySelector('.popup-close'),
+            popupContent = popupCheck.querySelector('.popup-content');
+      
+        checkBtn.forEach((elem) => {
+            elem.addEventListener('click', () => {
+                popupCheck.style.display = 'block';
+            });
+        });
+
+        popupClose.addEventListener('click', () => {
+            popupCheck.style.display = 'none';
+            event.preventDefault();
+
+        });
+        popupCheck.addEventListener('click', (event) => {
+            let target = event.target;
+            if (target.classList.contains('popup-close')) {
+                popupCheck.style.display = 'none';
+                popupContent.removeAttribute('style');
+            } else {
+                target = target.closest('.popup-content');
+                if (!target) {
+                    popupCheck.style.display = 'none';
+                }
+            }
+        });
+
+    };
+    check();
+
     // аккордеон 2
 
     const accordionTwo = () => {
-   
+
         document.addEventListener('click', (event) => {
             const panelHeading = event.target.closest('.panel-heading');
             if (panelHeading) {
@@ -49,39 +85,21 @@ window.addEventListener('DOMContentLoaded', () => {
                     now.classList.toggle('in', now === collapse && !now.classList.contains('in'));
                 });
             }
+            //     const  btn = document.querySelectorAll('.button.construct-btn');    
+            //     btn.forEach((item) => {
+            //     item.addEventListener('click', () => {
+            //     let target = event.target;
+            //     console.log(target);
+            //     });
+            //     }); 
+
         });
-    
+
     };
 
     accordionTwo();
 
-      // аккордеон калькулятор
-
-      const accordionCalc = () => {
-   
-        document.addEventListener('click', (event) => {
-            const panelHeading = event.target.closest('.panel-heading');
-            if (panelHeading) {
-                event.preventDefault();
-                const collapse = panelHeading.nextElementSibling;
-                panelHeading.closest('.panel-group').querySelectorAll('.panel-default').forEach(now => {
-                    now.classList.toggle('in', now === collapse && !now.classList.contains('in'));
-                });
-            }
-        });
-    
-    };
-
-    accordionCalc();
-
-
-
-
-
-
-
-
-
+  
 
 
 

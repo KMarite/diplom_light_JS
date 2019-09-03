2c83fecbf178b9831fdfa9456677a015723c9d4d
Marina <kravchenko.marite@gmail.com>
2019-09-03 21:17:20 +0300

    сделана кнопка больше и добавлен вызов еще одного модального окна со скидками

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
             popupCall = document.querySelector('.popup-call'),
             popupClose = document.querySelector('.popup-close'),
             popupContent = popupCall.querySelector('.popup-content');
-       
+
         callBtn.forEach((elem) => {
             elem.addEventListener('click', () => {
                 popupCall.style.display = 'block';
@@ -36,15 +36,15 @@ window.addEventListener('DOMContentLoaded', () => {
     };
     togglePopup();
 
-     // модальнoe окнo popup-check
+    // модальнoe окнo popup-check
 
-     const check = () => {
+    const check = () => {
 
         const checkBtn = document.querySelectorAll('.check-btn'),
             popupCheck = document.querySelector('.popup-check'),
             popupClose = popupCheck.querySelector('.popup-close'),
             popupContent = popupCheck.querySelector('.popup-content');
-      
+
         checkBtn.forEach((elem) => {
             elem.addEventListener('click', () => {
                 popupCheck.style.display = 'block';
@@ -72,6 +72,43 @@ window.addEventListener('DOMContentLoaded', () => {
     };
     check();
 
+    // модальнoe окнo popup-discount
+
+    const discount = () => {
+
+        const discountBtn = document.querySelectorAll('.discount-btn'),
+            popupDiscount = document.querySelector('.popup-discount'),
+            popupClose = popupDiscount.querySelector('.popup-close'),
+            popupContent = popupDiscount.querySelector('.popup-content');
+
+        discountBtn.forEach((elem) => {
+            elem.addEventListener('click', () => {
+                popupDiscount.style.display = 'block';
+            });
+        });
+
+        popupClose.addEventListener('click', () => {
+            popupDiscount.style.display = 'none';
+            event.preventDefault();
+
+        });
+        popupDiscount.addEventListener('click', (event) => {
+            let target = event.target;
+            if (target.classList.contains('popup-close')) {
+                popupDiscount.style.display = 'none';
+                popupContent.removeAttribute('style');
+            } else {
+                target = target.closest('.popup-content');
+                if (!target) {
+                    popupDiscount.style.display = 'none';
+                }
+            }
+        });
+
+    };
+    discount();
+
+
     // аккордеон 2
 
     const accordionTwo = () => {
@@ -84,22 +121,27 @@ window.addEventListener('DOMContentLoaded', () => {
                 panelHeading.closest('.panel-group').querySelectorAll('.panel-collapse').forEach(now => {
                     now.classList.toggle('in', now === collapse && !now.classList.contains('in'));
                 });
-            }
-            //     const  btn = document.querySelectorAll('.button.construct-btn');    
-            //     btn.forEach((item) => {
-            //     item.addEventListener('click', () => {
-            //     let target = event.target;
-            //     console.log(target);
-            //     });
-            //     }); 
-
+            }          
         });
-
     };
 
     accordionTwo();
 
-  
+    // кнопка больше
+
+    const buttonMore = () => {
+        const buttonAdd = document.querySelector('.add-sentence-btn');
+
+        buttonAdd.addEventListener('click', (event) => {
+            event.target.parentNode.querySelectorAll('.hidden, .visible-sm-block').forEach(i => {
+                i.classList.remove('hidden');
+                i.classList.remove('visible-sm-block');
+            });
+            buttonAdd.style.display = 'none';
+        });
+
+    };
+    buttonMore();
 
 
 
